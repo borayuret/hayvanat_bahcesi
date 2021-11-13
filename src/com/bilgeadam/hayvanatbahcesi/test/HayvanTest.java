75075593b9ddfcf04a668d494591eb49b097b6d8
Bora Yüret <borayuret@gmail.com>
2021-11-13 13:32:45 +0300

    hb

---
+++ src/com/bilgeadam/hayvanatbahcesi/test/HayvanTest.java
@@ -0,0 +1,21 @@
+package com.bilgeadam.hayvanatbahcesi.test;//package com.bilgeadam.hayvanatbahcesi.test;
+//
+//import com.bilgeadam.hayvanatbahcesi.Hayvan;
+//
+//public class HayvanTest {
+//
+//    public static void main(String[] args) {
+//        Hayvan h1 = new Hayvan();
+//        h1.sesCikar();
+//
+//        // agirlik getir
+//        System.out.println(h1.getAgirlik());
+//
+//        //ağırlık değiştir
+//        h1.setAgirlik(20);
+//
+//        // agirlik getir
+//        System.out.println(h1.getAgirlik());
+//    }
+//
+//}
